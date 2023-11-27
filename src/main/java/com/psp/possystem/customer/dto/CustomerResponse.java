25829f69ec64507135593635d6eb4a36f2fa4a39
Danielius <100567065+Danielius4@users.noreply.github.com>
2023-11-27 16:58:41 +0200

    added customer and discount api best case scenario documentation

---
+++ src/main/java/com/psp/possystem/customer/dto/CustomerResponse.java
@@ -0,0 +1,11 @@
+package com.psp.possystem.customer.dto;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class CustomerResponse {
+    public int ID = 2;
+    public String name = "name";
+    public String address = "address";
+    public String email = "email";
+}

d1f07693f176faae4a37b0d041e34fd18b5f393c
Na-Ga-TIV <anton.antonio.10@mail.ru>
2023-11-28 01:03:16 +0200

    Add DTO/Controllers for Client/Payments

---
+++ src/main/java/com/psp/possystem/payment/dto/PointResponse.java
@@ -0,0 +1,9 @@
+package com.psp.possystem.payment.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class PointResponse {
+    public double pointGained;
+    public double pointsSpent;
+}
---
+++ src/main/java/com/psp/possystem/payment/dto/TaxesResponse.java
@@ -0,0 +1,10 @@
+package com.psp.possystem.payment.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class TaxesResponse {
+    public double salesTax;
+    public double incomeTax;
+
+}
